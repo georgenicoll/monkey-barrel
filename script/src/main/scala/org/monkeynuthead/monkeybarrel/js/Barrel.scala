151a8e781a94c2b3a13b9156ed728e6aa47873cc
George Nicoll <george.nicoll@gmail.com>
2015-03-07 19:37:52 +0000

    Noddy Chart (using dimple for now) and try calling out to page script

--- script/src/main/scala/org/monkeynuthead/monkeybarrel/js/Barrel.scala
+++ script/src/main/scala/org/monkeynuthead/monkeybarrel/js/Barrel.scala
@@ -4,7 +4,8 @@ import org.monkeynuthead.monkeybarrel.core.{Aggregator, Row, Table}
 import org.scalajs.jquery._
 import rx.core.{Obs, Var}
 
-import scala.scalajs.js.JSApp
+import scala.scalajs.js
+import scala.scalajs.js.{Object, JSApp}
 import scalatags.Text.all._
 
 /**
@@ -19,6 +20,8 @@ object Barrel extends JSApp {
   private[this] val Population = "Pop"
 
   private[this] val Attributes = List(Country, City)
+
+  private[this] val Missing = "<Missing>"
 
   private[this] val table = Table(Attributes, List(
     Row(Map(Country -> "UK", City -> "London", Size -> "Big"), Map(Population -> 14100000)),
@@ -38,16 +41,27 @@ object Barrel extends JSApp {
   private[this] val DataDivId = "data"
   private[this] val AggSelectId = "aggregationAttribute"
 
-  private[this] def showTable(tableOpt: Option[Table]): Unit = {
-    jQuery(s"#${DataDivId}").empty()
+  private[this] def tableChanged(tableOpt: Option[Table]): Unit = {
+    jQuery(s"#$DataDivId").empty()
     val ps = tableOpt.map { tab =>
+
+      // Update the chart
+      val attribute = tab.attributes.head
+      val objs: Seq[js.Object] = tab.rows.map { row =>
+        js.Dynamic.literal(attribute -> js.Any.fromString(row.attributes.getOrElse(attribute, Missing)),
+          Population -> js.Any.fromDouble(row.measures.getOrElse(Population, 0d)))
+      }
+      val data: js.Array[js.Object] = js.Array(objs: _*)
+      js.Dynamic.global.createChart(attribute, Population, data)
+
+      // Construct the paragraph for the data
       tab.rows.map { row =>
         p(label(s"${tab.attributes.map(a => (a, row.attributes(a))).mkString(",")} = ${row.measures(Population)}"))
       }
-    }
-    val d = div(ps.toSeq)
+    }.toSeq
+    val d = div(ps)
     println(d.render)
-    jQuery(d.render).appendTo(s"#${DataDivId}")
+    jQuery(d.render).appendTo(s"#$DataDivId")
   }
 
   private[this] def aggregate(attribute: String): Unit = {
@@ -55,7 +69,7 @@ object Barrel extends JSApp {
   }
 
   private[this] def selectChanged(event: JQueryEventObject): Unit = {
-    val selected = jQuery(s"#${AggSelectId}").find("option:selected")
+    val selected = jQuery(s"#$AggSelectId").find("option:selected")
     println(s"select changed: ${selected.text()}")
     aggregate(selected.text())
   }
@@ -76,10 +90,10 @@ object Barrel extends JSApp {
       ))
     )
     jQuery(para.render).appendTo(jQuery("body"))
-    jQuery(attributeSelect.render).change(selectChanged _).appendTo(jQuery(s"#${ControlDivId}"))
+    jQuery(attributeSelect.render).change(selectChanged _).appendTo(jQuery(s"#$ControlDivId"))
 
-    Obs(aggregated) {
-      showTable(aggregated())
+    Obs(aggregated, skipInitial = true) {
+      tableChanged(aggregated())
     }
 
     aggregate(Country)
