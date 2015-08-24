fced634d94d6a847b7981110fff36cf4510eeac0
George Nicoll <george.nicoll@gmail.com>
2015-08-24 22:12:19 +0100

    Recognise an error, recognise web socket stopped

--- web/src/main/scala/org/monkeynuthead/monkeybarrel/web/Services.scala
+++ web/src/main/scala/org/monkeynuthead/monkeybarrel/web/Services.scala
@@ -4,7 +4,8 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.ws.{TextMessage, Message}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.Materializer
-import akka.stream.scaladsl.Flow
+import akka.stream.scaladsl.{Source, Flow}
+import akka.stream.stage.{TerminationDirective, SyncDirective, Context, PushStage}
 import akkahttptwirl.TwirlSupport
 import org.monkeynuthead.monkeybarrel.web.Model.HelloResult
 import scala.concurrent.ExecutionContextExecutor
@@ -37,18 +38,61 @@ trait Services extends MicroPickleSupport with TwirlSupport {
       }
     }
   } ~
+  path("echo" / Segment) { seg =>
+    handleWebsocketMessages(echoInUppercaseFlow(seg))
+  } ~
   path("echo") {
-    handleWebsocketMessages(echoInUppercaseFlow())
+    handleWebsocketMessages(echoInUppercaseFlow("Undefined"))
   } ~
   path("scripts" / Segment) { segment =>
     getFromResource(segment)
   }
 
-  def echoInUppercaseFlow(): Flow[Message, Message, Unit] = {
-    val websockflow = Flow[Message].map {
-      case m: TextMessage => TextMessage(m.textStream.map(_.toUpperCase()))
-      case a: Message => a
+  type SourceString = Source[String, _]
+
+  //Temporary noddy stream proccessing
+  def toUpperCaseFlow: Flow[Option[SourceString],Option[SourceString],Unit] = {
+    Flow[Option[SourceString]].map {
+      _.map(_.map(_.toUpperCase()))
     }
+  }
+
+  //Ashamedly copied from https://github.com/jrudolph/akka-http-scala-js-websocket-chat/
+  def reportErrorsFlow[T](identifier: String): Flow[T, T, Unit] = {
+    Flow[T].transform { () =>
+      new PushStage[T, T] {
+        override def onPush(elem: T, ctx: Context[T]): SyncDirective = ctx.push(elem)
+
+        override def onUpstreamFailure(cause: Throwable, ctx: Context[T]): TerminationDirective = {
+          println(s"WebSocket '$identifier' failed with cause: $cause")
+          super.onUpstreamFailure(cause, ctx)
+        }
+
+
+        @throws[Exception](classOf[Exception])
+        override def postStop(): Unit = {
+          println(s"WebSocket '$identifier' stopped.")
+          super.postStop()
+        }
+
+      }
+    }
+  }
+
+  def echoInUppercaseFlow(identifier: String): Flow[Message, Message, Unit] = {
+    println(s"Constructing web socket flow for '$identifier'")
+    //Go via another flow - this will be replaced with something useful, for the moment just use the uppercase flow
+    val websockflow = Flow[Message].map {
+      case m: TextMessage => Some(m.textStream)
+      case _ => None
+    }
+    .via(toUpperCaseFlow)
+    .map {
+      case Some(text) => TextMessage(text)
+      case None => TextMessage("Failed in Processing")
+    }
+    .via(reportErrorsFlow(identifier))
+
     websockflow
   }
 
