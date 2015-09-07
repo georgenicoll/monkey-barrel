f67a2d4de993d3f7c9742294ca80d718966e80f8
George Nicoll <george.nicoll@gmail.com>
2015-09-07 20:45:03 +0100

    Tie down access modifiers

--- web/src/main/scala/org/monkeynuthead/monkeybarrel/web/Services.scala
+++ web/src/main/scala/org/monkeynuthead/monkeybarrel/web/Services.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.ws.{TextMessage, Message}
 import akka.http.scaladsl.server.Directives._
 import akka.stream.Materializer
-import akka.stream.scaladsl.{Source, Flow}
+import akka.stream.scaladsl.{Sink, Source, Flow}
 import akka.stream.stage.{TerminationDirective, SyncDirective, Context, PushStage}
 import akkahttptwirl.TwirlSupport
 import org.monkeynuthead.monkeybarrel.web.Model.HelloResult
@@ -51,14 +51,14 @@ trait Services extends MicroPickleSupport with TwirlSupport {
   type SourceString = Source[String, _]
 
   //Temporary noddy stream proccessing
-  def toUpperCaseFlow: Flow[Option[SourceString],Option[SourceString],Unit] = {
+  private[this] def toUpperCaseFlow: Flow[Option[SourceString],Option[SourceString],Unit] = {
     Flow[Option[SourceString]].map {
       _.map(_.map(_.toUpperCase()))
     }
   }
 
   //Ashamedly copied from https://github.com/jrudolph/akka-http-scala-js-websocket-chat/
-  def reportErrorsFlow[T](identifier: String): Flow[T, T, Unit] = {
+  private[this] def reportErrorsFlow[T](identifier: String): Flow[T, T, Unit] = {
     Flow[T].transform { () =>
       new PushStage[T, T] {
         override def onPush(elem: T, ctx: Context[T]): SyncDirective = ctx.push(elem)
@@ -79,7 +79,7 @@ trait Services extends MicroPickleSupport with TwirlSupport {
     }
   }
 
-  def echoInUppercaseFlow(identifier: String): Flow[Message, Message, Unit] = {
+  private[this] def echoInUppercaseFlow(identifier: String): Flow[Message, Message, Unit] = {
     println(s"Constructing web socket flow for '$identifier'")
     //Go via another flow - this will be replaced with something useful, for the moment just use the uppercase flow
     val websockflow = Flow[Message].map {
