c1eb2d5d135712c2044d8c7cb05c690e2c61e04a
George Nicoll <george.nicoll@gmail.com>
2015-10-19 18:51:50 +0100

    Tidy up tests and add multi attribute test

--- stream-processing/src/test/scala/com/monkeynuthead/streams/WindowSpec.scala
+++ stream-processing/src/test/scala/com/monkeynuthead/streams/WindowSpec.scala
@@ -23,15 +23,19 @@ class WindowByAttributeSpec extends WordSpec with MustMatchers with BeforeAndAft
 
   import Windows._
 
+  private def generateGraph(byAttributesFlow: Flow[DataType, DataType, _]): RunnableGraph[(ActorRef, Future[scala.List[Windows.DataType]])] = {
+    val maps = Source.actorRef[DataType](10, OverflowStrategy.dropTail)
+    val flow = maps.via(byAttributesFlow)
+    val sink: Sink[DataType, Future[List[DataType]]] = Sink.fold(List.newBuilder[DataType]) { (b, data: DataType) =>
+      b += data
+    }.mapMaterializedValue(_.map(_.result()))
+    flow.toMat(sink)(Keep.both)
+  }
+
   "window by attribute" must {
 
     "push out each event when no aggregation" in {
-      val maps = Source.actorRef[DataType](10, OverflowStrategy.dropTail)
-      val flow = maps.via(byAttributes(Flow[DataType], None))
-      val sink: Sink[DataType, Future[List[DataType]]] = Sink.fold(List.newBuilder[DataType]) { (b, data: DataType) =>
-        b += data
-      }.mapMaterializedValue(_.map(_.result()))
-      val graph = flow.toMat(sink)(Keep.both)
+      val graph = generateGraph(byAttributes(Flow[DataType], None))
       val (actor, futureList) = graph.run()
 
       val m1 = Map("George" -> "Nicoll")
@@ -45,20 +49,14 @@ class WindowByAttributeSpec extends WordSpec with MustMatchers with BeforeAndAft
       result must equal(List(m1, m2))
     }
 
+    val HowMuchMapping: MappingFunction = (optExisting: Option[DataType], newData: DataType) => {
+      val existingNum = optExisting.flatMap(_.get("HowMuch")).map(_.asInstanceOf[Double]).getOrElse(0d)
+      val newNum = newData.get("HowMuch").map(_.asInstanceOf[Double]).getOrElse(0d)
+      Map("HowMuch" -> (existingNum + newNum))
+    }
+
     "'aggregate' like events" in {
-
-      val howMuchMapping: MappingFunction = (optExisting: Option[DataType], newData: DataType) => {
-        val existingNum = optExisting.flatMap(_.get("HowMuch")).map(_.asInstanceOf[Double]).getOrElse(0d)
-        val newNum = newData.get("HowMuch").map(_.asInstanceOf[Double]).getOrElse(0d)
-        Map("HowMuch" -> (existingNum + newNum))
-      }
-
-      val maps = Source.actorRef[DataType](50, OverflowStrategy.fail)
-      val flow = maps.via(byAttributes(Flow[DataType], Some(howMuchMapping), "George"))
-      val sink: Sink[DataType, Future[List[DataType]]] = Sink.fold(List.newBuilder[DataType]) { (b, data: DataType) =>
-        b += data
-      }.mapMaterializedValue(_.map(_.result()))
-      val graph = flow.toMat(sink)(Keep.both)
+      val graph = generateGraph(byAttributes(Flow[DataType], Some(HowMuchMapping), "George"))
       val (actor, futureList) = graph.run()
 
       val m1 = Map("George" -> "Nicoll", "Monkey" -> "Hanger", "HowMuch" -> 56d)
@@ -78,6 +76,30 @@ class WindowByAttributeSpec extends WordSpec with MustMatchers with BeforeAndAft
         Map("George" -> "Formby", "HowMuch" -> 12d),
         Map("George" -> "Nicoll", "HowMuch" -> 80d),
         Map("George" -> "Formby", "HowMuch" -> 6d)
+      ))
+    }
+
+    "'aggregate' using multiple attributes" in {
+      val graph = generateGraph(byAttributes(Flow[DataType], Some(HowMuchMapping), "George", "Monkey"))
+      val (actor, futureList) = graph.run()
+
+      val m1 = Map("George" -> "Nicoll", "Monkey" -> "Hanger", "HowMuch" -> 56d)
+      val m2 = Map("George" -> "Formby", "Monkey" -> "Lumpy", "HowMuch" -> 12d)
+      val m3 = Map("George" -> "Nicoll", "Monkey" -> "Hanger", "HowMuch" -> 24d)
+      val m4 = Map("George" -> "Formby", "Monkey" -> "Bumpy", "HowMuch" -> -6d)
+
+      actor ! m1
+      actor ! m2
+      actor ! m3
+      actor ! m4
+      actor ! Success("Done")
+
+      val result = Await.result(futureList, 3 seconds)
+      result must equal(List(
+        Map("George" -> "Nicoll", "Monkey" -> "Hanger", "HowMuch" -> 56d),
+        Map("George" -> "Formby", "Monkey" -> "Lumpy", "HowMuch" -> 12d),
+        Map("George" -> "Nicoll", "Monkey" -> "Hanger", "HowMuch" -> 80d),
+        Map("George" -> "Formby", "Monkey" -> "Bumpy", "HowMuch" -> -6d)
       ))
     }
 
